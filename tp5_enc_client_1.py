d3e696816100b7ce67a406873291d92139d41f32
vbrendan <brendan.visine@ynov.com>
2024-10-24 12:02:09 +0200

    TP5

--- tp5_enc_client_1.py
+++ tp5_enc_client_1.py
@@ -7,7 +7,7 @@ s.send('Hello'.encode())
 
 # On reçoit la string Hello
 data = s.recv(1024)
-print(data.encode())
+print(data.decode())
 
 # Récupération d'une string utilisateur
 msg = input("Calcul à envoyer: ")
