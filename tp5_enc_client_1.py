bd5beceb3baee0ed9dd31c3150c890597433ae83
vbrendan <brendan.visine@ynov.com>
2024-10-24 10:43:55 +0200

    TP5

--- tp5_enc_client_1.py
+++ tp5_enc_client_1.py
@@ -19,6 +19,19 @@ if match:
     nombre2 = int(match.group(3))
     if -1048575 <= nombre1 <= 1048575 and -1048575 <= nombre2 <= 1048575:
         print(f"Expression valide : {nombre1} {operateur} {nombre2}")
+        encoded_msg = msg.encode('utf-8')
+
+        # on calcule sa taille, en nombre d'octets
+        msg_len = len(encoded_msg)
+
+        # on encode ce nombre d'octets sur une taille fixe de 4 octets
+        header = msg_len.to_bytes(4, byteorder='big')
+
+        # on peut concaténer ce header avec le message, avant d'envoyer sur le réseau
+        payload = header + encoded_msg
+
+        # on peut envoyer ça sur le réseau
+        sock.send(payload)
     else:
         print("Erreur : Les nombres doivent être entre -1048575 et +1048575.")
 else:
