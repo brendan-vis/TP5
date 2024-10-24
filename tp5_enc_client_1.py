d7d9b2576e61e47dc59ce2527f30a755b8c2d847
vbrendan <brendan.visine@ynov.com>
2024-10-24 10:34:05 +0200

    TP5

--- tp5_enc_client_1.py
+++ tp5_enc_client_1.py
@@ -1,4 +1,5 @@
 import socket
+import re
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('10.1.1.11', 13337))
@@ -9,6 +10,21 @@ data = s.recv(1024)
 
 # Récupération d'une string utilisateur
 msg = input("Calcul à envoyer: ")
+
+motif = r"^\s*(-?\d{1,7})\s*([\+\-\*])\s*(-?\d{1,7})\s*$"
+match = re.match(motif, msg)
+if match:
+    nombre1 = int(match.group(1))
+    operateur = match.group(2)
+    nombre2 = int(match.group(3))
+    if -1048575 <= nombre1 <= 1048575 and -1048575 <= nombre2 <= 1048575:
+        print(f"Expression valide : {nombre1} {operateur} {nombre2}")
+        encoded_msg = msg.encode('utf-8')
+        print(f"{encoded_msg}")
+    else:
+        print("Erreur : Les nombres doivent être entre -1048575 et +1048575.")
+else:
+    print("Erreur : Format incorrect. Utilisez la forme 'x opérateur y' avec +, -, ou *.")
 
 # On envoie
 s.send(msg.encode())
