cdc1a6948b7fa0f1ec028efc171a8c7d3c461a62
vbrendan <brendan.visine@ynov.com>
2024-10-24 12:49:02 +0200

    TP5

--- tp5_enc_client_1.py
+++ tp5_enc_client_1.py
@@ -3,11 +3,6 @@ import re
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('10.1.1.11', 13337))
-s.send('Hello'.encode())
-
-# On reçoit la string Hello
-data = s.recv(1024)
-print(data.decode())
 
 # Récupération d'une string utilisateur
 msg = input("Calcul à envoyer: ")
